142835a5e481148f467ed8add9c533789e743e11
remco van der Velde <remco.pc@outlook.com>
2024-10-21 16:54:04 +0200

    deno installation

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -17,8 +17,10 @@ trait Main {
     public function deno_install(): void
     {
         Core::interactive();
-        $object = $this->object();
-        $command = 'cp ' . $object->config('framework.dir.vendor') . 'raxon/deno/Data/deno /usr/local/bin/deno';
+        $command = 'curl -fsSL https://deno.land/install.sh | sh';
+        exec($command, $output);
+        echo implode(PHP_EOL, $output) . PHP_EOL;
+        $command = 'cp ~/.deno/bin/deno /usr/local/bin/deno';
         exec($command, $output);
         echo implode(PHP_EOL, $output) . PHP_EOL;
         $command = 'deno --version';
