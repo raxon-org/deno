517fab6f047c449f73fec1c2dde0edcd346229b2
remco van der Velde <remco.pc@outlook.com>
2024-10-21 17:29:39 +0200

    deno installation

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -21,6 +21,9 @@ trait Main {
 //        exec($command, $output);
 //        echo implode(PHP_EOL, $output) . PHP_EOL;
         $object = $this->object();
+        $command = 'rm ~/deno.zip';
+        exec($command, $output);
+        echo implode(PHP_EOL, $output) . PHP_EOL;
         $command = 'cp ' . $object->config('project.dir.vendor') . 'raxon/deno/src/Data/deno.zip ~/deno.zip';
         exec($command, $output);
         echo implode(PHP_EOL, $output) . PHP_EOL;
